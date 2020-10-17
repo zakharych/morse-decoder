eea04f513af6e83dc4cb451eb412bd7bf273e1e3
Alex <alexzlkl@yandex.ru>
2020-10-17 23:57:31 +0300

    feat: decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let n;
+    let world = "";
+    let dot = 10;
+    let dashes = 11;
+    for (let i = 0; i < expr.length; i += 10) {
+        let strSimnol = expr.slice(i, (n = i + 10));
+        if (strSimnol[1] == "*") {
+            space = " ";
+            world += space;
+        } else {
+            let s;
+            let letter = "";
+            for (let k = 0; k < strSimnol.length; k += 2) {
+                let key = strSimnol.slice(k, (s = k + 2));
+                switch (key) {
+                    case "00":
+                        letter = letter;
+                        break;
+                    case "10":
+                        letter += ".";
+                        break;
+                    case "11":
+                        letter += "-";
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            for (var code in MORSE_TABLE) {
+                var value = MORSE_TABLE[code];
+                if (code == letter) {
+                    world += value;
+                    break;
+                }
+            }
+        }
+    }
+    return world
 }
 
 module.exports = {
